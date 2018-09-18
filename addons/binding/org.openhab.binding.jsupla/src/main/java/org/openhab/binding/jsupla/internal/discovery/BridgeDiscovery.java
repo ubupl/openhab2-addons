cfa027d259434cbd7d5ee6e45bb7ffd43c1e127b
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-09-18 15:51:06 +0200

    Not generating password and access id in BridgeDiscovery.java

--- addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/internal/discovery/BridgeDiscovery.java
+++ addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/internal/discovery/BridgeDiscovery.java
@@ -14,11 +14,8 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 import java.util.Random;
 
-import static java.lang.String.format;
 import static java.lang.String.valueOf;
 import static org.openhab.binding.jsupla.jSuplaBindingConstants.CONFIG_PORT;
-import static org.openhab.binding.jsupla.jSuplaBindingConstants.CONFIG_SERVER_ACCESS_ID;
-import static org.openhab.binding.jsupla.jSuplaBindingConstants.CONFIG_SERVER_ACCESS_ID_PASSWORD;
 import static org.openhab.binding.jsupla.jSuplaBindingConstants.DEFAULT_PORT;
 import static org.openhab.binding.jsupla.jSuplaBindingConstants.JSUPLA_SERVER_TYPE;
 
@@ -43,33 +40,19 @@ public class BridgeDiscovery extends AbstractDiscoveryService {
     }
 
     private void discover() {
-        final int accessId = randomAccessId();
-        final String accessIdPassword = randomAccessIdPassword();
-
         ThingUID thingUID = new ThingUID(JSUPLA_SERVER_TYPE, valueOf(DEFAULT_PORT));
-        final String label = format("jSupla Server (port: %s, server access ID: %s, server access ID password: %s)",
-                DEFAULT_PORT, accessId, accessIdPassword);
+        final String label = "jSupla Server";
         final DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID)
-                                                        .withProperties(buildThingProperties(accessId, accessIdPassword))
+                                                        .withProperties(buildThingProperties())
                                                         .withLabel(label)
                                                         .build();
         logger.debug("Adding server to discovery; {}", label);
         thingDiscovered(discoveryResult);
     }
 
-    private Map<String, Object> buildThingProperties(int serverAccessId, String serverAccessIdPassword) {
+    private Map<String, Object> buildThingProperties() {
         return ImmutableMap.<String, Object>builder()
-                       .put(CONFIG_SERVER_ACCESS_ID, serverAccessId)
-                       .put(CONFIG_SERVER_ACCESS_ID_PASSWORD, serverAccessIdPassword)
                        .put(CONFIG_PORT, DEFAULT_PORT)
                        .build();
-    }
-
-    private int randomAccessId() {
-        return random.nextInt(4_000) + 1_000;
-    }
-
-    private String randomAccessIdPassword() {
-        return valueOf(random.nextInt(4_000) + 1_000);
     }
 }
