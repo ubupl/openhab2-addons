a9709ebda7786d54120306bb2f99a0d74360c5a3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-09-26 15:03:45 +0200

    Adding licence

--- addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/handler/JSuplaCloudBridgeHandler.java
+++ addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/handler/JSuplaCloudBridgeHandler.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ * <p>
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.jsupla.handler;
 
 import io.netty.handler.ssl.SslContext;
@@ -140,7 +148,7 @@ public class JSuplaCloudBridgeHandler extends BaseBridgeHandler {
                 channel,
                 scheduledPool,
                 suplaDeviceRegistry);
-        
+
         channel.getMessagePipe().subscribe(
                 jSuplaChannel::onNext,
                 jSuplaChannel::onError,
--- addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/handler/SuplaDeviceHandler.java
+++ addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/handler/SuplaDeviceHandler.java
@@ -1,14 +1,10 @@
 /**
- * Copyright (c) 2014,2018 by the respective copyright holders.
- * <p>
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- * <p>
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- * <p>
- * SPDX-License-Identifier: EPL-2.0
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.openhab.binding.jsupla.handler;
 
