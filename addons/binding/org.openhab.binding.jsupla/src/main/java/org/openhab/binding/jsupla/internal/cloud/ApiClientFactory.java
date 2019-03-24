740c04da91e9600933e2d24e5e2586802ea396e6
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-03-24 19:08:02 +0100

    Add logging capacity

--- addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/internal/cloud/ApiClientFactory.java
+++ addons/binding/org.openhab.binding.jsupla/src/main/java/org/openhab/binding/jsupla/internal/cloud/ApiClientFactory.java
@@ -1,5 +1,6 @@
 package org.openhab.binding.jsupla.internal.cloud;
 
+import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 import org.slf4j.Logger;
 import pl.grzeslowski.jsupla.api.generated.ApiClient;
 
@@ -10,7 +11,7 @@ public class ApiClientFactory {
     public ApiClient newApiClient(String token, Logger logger) {
         final ApiClient apiClient = pl.grzeslowski.jsupla.api.ApiClientFactory.INSTANCE.newApiClient(token);
         if (logger != null) {
-            apiClient.setDebugging(logger.isDebugEnabled());
+            apiClient.setDebugging(logger.isDebugEnabled(), new HttpLoggingInterceptor(logger::trace));
         }
         return apiClient;
     }
